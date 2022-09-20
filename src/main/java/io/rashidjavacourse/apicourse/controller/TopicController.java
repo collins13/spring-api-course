dfb76faf21c6f5627f9be05bc3f42c87d6c270d7
collins13 <rashidcollins16@gmail.com>
2022-09-20 09:27:25 +0300

    cleaning up some changes

--- src/main/java/io/rashidjavacourse/apicourse/controller/TopicController.java
+++ src/main/java/io/rashidjavacourse/apicourse/controller/TopicController.java
@@ -1,20 +1,15 @@
 package io.rashidjavacourse.apicourse.controller;
 import org.springframework.web.bind.annotation.RestController;
-
 import io.rashidjavacourse.apicourse.Models.Topic;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.Arrays;
 import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class TopicController{
 
-    @GetMapping("/topics")
+    @RequestMapping("/topics")
         public List<Topic> getAllTopics(){
             return Arrays.asList(
                 new Topic("spring", "spring boot", "spring boot description"),
